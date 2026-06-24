d7a5a7fa1b54f6213e8e6806e10a688337a683b1
chittybb1357-commits <chittybb1357@gmail.com>
2026-06-24 16:11:12 +0900

    Add uuid dependency and refactor App component to use UUID for article IDs

--- src/components/updateArticle.jsx
+++ src/components/updateArticle.jsx
@@ -2,14 +2,33 @@ import { useState } from "react";
 
 function UpdateArticle({ title, desc, onSubmit }) {
   console.log("UpdateArticle render");
-  const [newTitle, setNewTitle] = useState(title);
-  const [newDesc, setNewDesc] = useState(desc);
-  const handleTitleChange = e => {
-    setNewTitle(e.target.value);
+
+  // const [newTitle, setNewTitle] = useState(title);
+  // const [newDesc, setNewDesc] = useState(desc);
+
+  // const handleTitleChange = e => {
+  //   setNewTitle(e.target.value);
+  // };
+  // const handleDescChange = e => {
+  //   setNewDesc(e.target.value);
+  // };
+
+  const [content, setContent] = useState({
+    title: title,
+    desc: desc,
+  });
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+
+    setContent(prev => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
   };
-  const handleDescChange = e => {
-    setNewDesc(e.target.value);
-  };
+
   return (
     <>
       <h2>Update Article</h2>
@@ -17,7 +36,7 @@ function UpdateArticle({ title, desc, onSubmit }) {
         action=""
         onSubmit={e => {
           e.preventDefault();
-          onSubmit(e.target.title.value, e.target.desc.value);
+          onSubmit(content.title, content.desc);
         }}
       >
         <div>
@@ -26,13 +45,13 @@ function UpdateArticle({ title, desc, onSubmit }) {
             type="text"
             name="title"
             id="title"
-            value={newTitle}
-            onChange={handleTitleChange}
+            value={content.title}
+            onChange={handleChange}
           />
         </div>
         <div>
           <label htmlFor="desc">desc</label>
-          <textarea name="desc" id="desc" value={newDesc} onChange={handleDescChange}></textarea>
+          <textarea name="desc" id="desc" value={content.desc} onChange={handleChange}></textarea>
         </div>
         <button>Submit</button>
       </form>
